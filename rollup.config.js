08964602d2deddbadf87803d11a562842a4536fe
Hoyeung <hoyeungw@outlook.com>
2021-12-14 16:55:57 +0800

    use @babel/preset-env node 16

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import commonjs            from '@rollup/plugin-commonjs'
 import json                from '@rollup/plugin-json'
 import nodeResolve         from '@rollup/plugin-node-resolve'
 import { decoObject, ros } from '@spare/logger'
-import babel               from 'rollup-plugin-babel'
+import babel               from '@rollup/plugin-babel'
 import fileInfo            from 'rollup-plugin-fileinfo'
 
 const { name, dependencies, main, module } = require(process.cwd() + '/package.json')
@@ -26,17 +26,17 @@ export default [
         comments: true,
         sourceMap: true,
         exclude: 'node_modules/**',
-        // babelHelpers: 'bundled',
+        babelHelpers: 'bundled',
         presets: [
-          [ '@babel/preset-env', { targets: { node: '14' } } ]
+          [ '@babel/preset-env', { targets: { node: '16' } } ]
         ],
         plugins: [
-          [ '@babel/plugin-proposal-optional-chaining' ],
-          [ '@babel/plugin-proposal-nullish-coalescing-operator' ],
+          // ['@babel/plugin-proposal-decorators', { legacy: true }],
+          // [ '@babel/plugin-proposal-optional-chaining' ],
+          // [ '@babel/plugin-proposal-nullish-coalescing-operator' ],
           [ '@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' } ],
-          [ '@babel/plugin-proposal-class-properties', { loose: true } ],
-          [ '@babel/plugin-proposal-private-methods', { loose: true } ],
-          [ '@babel/plugin-transform-runtime', { helpers: false, } ]
+          // [ '@babel/plugin-proposal-private-methods', { loose: true } ],
+          // [ '@babel/plugin-transform-runtime', { helpers: false, } ]
         ]
       }),
       json(),
