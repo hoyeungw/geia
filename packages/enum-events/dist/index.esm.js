96e4cb08308af6034ce33ca34c96f5fa22ee423c
Hoyeung <hoyeungw@outlook.com>
2021-12-15 10:58:54 +0800

    update dependencies

--- packages/enum-events/dist/index.esm.js
+++ packages/enum-events/dist/index.esm.js
@@ -9,11 +9,16 @@ const MESSAGE = 'message',
       STATUS = 'status',
       ONLINE = 'online';
 const CLOSE = 'close',
+      DATA = 'data',
+      END = 'end',
       ERROR = 'error',
+      PAUSE = 'pause',
+      READABLE = 'readable',
+      RESUME = 'resume',
       EXIT = 'exit',
       EXCEPTION = 'exception',
       DISCONNECT = 'disconnect',
       UNCAUGHT_EXCEPTION = 'uncaughtException',
       WARNING = 'warning';
 
-export { CLOSE, CONNECT, CONNECTION, DISCONNECT, DONE, ERROR, EXCEPTION, EXIT, FORK, LISTENING, MESSAGE, ONLINE, READY, STATUS, TASKED, UNCAUGHT_EXCEPTION, WARNING };
+export { CLOSE, CONNECT, CONNECTION, DATA, DISCONNECT, DONE, END, ERROR, EXCEPTION, EXIT, FORK, LISTENING, MESSAGE, ONLINE, PAUSE, READABLE, READY, RESUME, STATUS, TASKED, UNCAUGHT_EXCEPTION, WARNING };
